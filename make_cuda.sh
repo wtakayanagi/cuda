146c6311c8a70dc8adf65ffcab8eb7c588bf9246
otakugeek <xjj59307@gmail.com>
2012-09-20 15:56:18 +0900

    update

--- make_cuda.sh
+++ make_cuda.sh
@@ -1,4 +1,3 @@
 # cat src/javascript.pegjs src/actor.pegjs > cuda.pegjs
 pegjs src/c.pegjs
-rm src/c.js .
 node parse.js
--- src/parse.js
+++ src/parse.js
@@ -1,8 +1,8 @@
 var fs = require('fs');
 var util = require('util');
-var js = require('./c');
+var js = require('c');
 
-fs.readFile('.example/sample.js',
+fs.readFile('../.example/sample.js',
     function (err, input) {
         input = '' + input;
         var tree = js.parse(input, 'start');
